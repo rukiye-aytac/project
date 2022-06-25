1d6e83d824cbf103278ef623e2cdcbb44cb9b183
rukiye-aytac <rkydzgn890@gmail.com>
2022-06-25 13:52:53 +0300

    first commit

---
+++ src/components/Main/Card.jsx
@@ -0,0 +1,14 @@
+const Card = (data) => {
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{data.title}</h1>
+      </div>
+      <img src={data.image} alt="" srcset="" />
+      <div className="card-over">
+        <p>{data.desc}</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
